6d9669e0c894167e18ec584e04f089288ef25f20
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-06-30 15:37:49 +0200

    correction

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cours d'informatique : Bases de PHP - Sessions et Formulaires</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Cours d'informatique : Bases de PHP - Sessions et Formulaires</h1>
+
+        <a href="./partie6/index.php">Variable d'URL</a>
+        <a href="./partie7/index.php">Formulaires</a>
+        <a href="./partie8/index.php">Sessions et cookies</a>
+        <a href="./partie9/index.php">Formats dates</a>
+    </div>
+</body>
+
+</html>
